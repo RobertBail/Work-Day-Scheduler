268e19bd5aeef791ecea98d2d72469c69f30d4fd
Robert Bailey <r.bail@hotmail.com>
2023-10-17 00:23:47 +1030

    scheduler changes

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ $('#currentDay').text(today.format('dddd, D MMMM YYYY'));
 function timecolor() {
   var currenttime = moment().hour();
 
-  
   $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
@@ -30,7 +29,7 @@ function timecolor() {
 
 function saveInput9(){     
 
-var userInput9 = document.getElementById('hour9').value.trim();
+var userInput9 = document.getElementById('hour9').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour9', JSON.stringify(userInput9));
 console.log(userInput9);
 
@@ -38,10 +37,9 @@ console.log(userInput9);
 
 document.getElementById('hour9').value = localStorage.getItem('hour9');
 
-
 function saveInput10(){
 
-var userInput10 = document.getElementById('hour10').value.trim();
+var userInput10 = document.getElementById('hour10').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour10', JSON.stringify(userInput10));
 console.log(userInput10);
 
@@ -51,7 +49,7 @@ document.getElementById('hour10').value = localStorage.getItem('hour10');
 
 function saveInput11(){
 
-var userInput11 = document.getElementById('hour11').value.trim();
+var userInput11 = document.getElementById('hour11').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour11', JSON.stringify(userInput11));
 console.log(userInput11);
 
@@ -61,7 +59,7 @@ document.getElementById('hour11').value = localStorage.getItem('hour11');
 
 function saveInput12(){
 
-var userInput12 = document.getElementById('hour12').value.trim();
+var userInput12 = document.getElementById('hour12').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour12', JSON.stringify(userInput12));
 console.log(userInput12);  
 
@@ -71,7 +69,7 @@ document.getElementById('hour12').value = localStorage.getItem('hour12');
 
 function saveInput13(){
 
-var userInput13 = document.getElementById('hour13').value.trim();
+var userInput13 = document.getElementById('hour13').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour13', JSON.stringify(userInput13));
 console.log(userInput13); 
 
@@ -81,7 +79,7 @@ document.getElementById('hour13').value = localStorage.getItem('hour13');
 
 function saveInput14(){
 
-var userInput14 = document.getElementById('hour14').value.trim();
+var userInput14 = document.getElementById('hour14').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour14', JSON.stringify(userInput14));
 console.log(userInput14);
 
@@ -91,7 +89,7 @@ document.getElementById('hour14').value = localStorage.getItem('hour14');
 
 function saveInput15(){
 
-var userInput15 = document.getElementById('hour15').value.trim();
+var userInput15 = document.getElementById('hour15').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour15', JSON.stringify(userInput15));
 console.log(userInput15); 
 
@@ -101,7 +99,7 @@ document.getElementById('hour15').value = localStorage.getItem('hour15');
 
 function saveInput16(){
 
-var userInput16 = document.getElementById('hour16').value.trim();
+var userInput16 = document.getElementById('hour16').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour16', JSON.stringify(userInput16));
 console.log(userInput16); 
 
@@ -111,7 +109,7 @@ document.getElementById('hour16').value = localStorage.getItem('hour16');
   
 function saveInput17(){
 
-var userInput17 = document.getElementById('hour17').value.trim();
+var userInput17 = document.getElementById('hour17').value.replace(/(\r\n|\n|\r)/gm," ");
 localStorage.setItem('hour17', JSON.stringify(userInput17));
 console.log(userInput17);  
 

6e554179cff2b8fee4f314b32ca8e85b366ced74
Robert Bailey <r.bail@hotmail.com>
2023-10-16 20:11:13 +1030

    scheduler changes

--- assets/script.js
+++ assets/script.js
@@ -1,92 +1,116 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var urlValue = window.location.href;
-$('#refresh').attr('href',urlValue);
+// in the html
 
 var today = dayjs();
-$('#currentDay').text(today.format('MMM D, YYYY'));
+$('#currentDay').text(today.format('D MMMM YYYY'));
+
+function timecolor() {
+var date = today;
+var hour = date.getHours();
+$('#currentHour').today.format('hh:mm:ss');
+var timeblock = document.getElementById('currentHour');
+
+
+if (hour >= 9 && hour <= 17) {
+  timeblock.style = 'present';
+} else if (hour < 17) {
+  timeblock.style = 'past';
+} else {
+  timeblock.style = 'future';
+}
+}
 
 function saveInput9(){     
 
-var userInput = document.getElementById('9').value.trim();
-localStorage.setItem('server', JSON.stringify(userInput));
-
-document.getElementById('9').value = localStorage.getItem('server');
+var userInput9 = document.getElementById('hour9').value.trim();
+localStorage.setItem('hour9', JSON.stringify(userInput9));
+console.log(userInput9);
 
 }
+
+document.getElementById('hour9').value = localStorage.getItem('hour9');
+
 
 function saveInput10(){
 
-var userInput = document.getElementById('10').value.trim();
-localStorage.setItem('server', JSON.stringify(userInput));
-
-document.getElementById('10').value = localStorage.getItem('server');
+var userInput10 = document.getElementById('hour10').value.trim();
+localStorage.setItem('hour10', JSON.stringify(userInput10));
+console.log(userInput10);
 
 }
 
+document.getElementById('hour10').value = localStorage.getItem('hour10');
+
 function saveInput11(){
 
-  var userInput = document.getElementById('11').value.trim();
-  localStorage.setItem('server', JSON.stringify(userInput));
-  
-  document.getElementById('11').value = localStorage.getItem('server');
-  
-  }
+var userInput11 = document.getElementById('hour11').value.trim();
+localStorage.setItem('hour11', JSON.stringify(userInput11));
+console.log(userInput11);
+
+}
+
+document.getElementById('hour11').value = localStorage.getItem('hour11');
 
 function saveInput12(){
 
-  var userInput = document.getElementById('12').value.trim();
-  localStorage.setItem('server', JSON.stringify(userInput));
-    
-  document.getElementById('12').value = localStorage.getItem('server');
-    
-  }
+var userInput12 = document.getElementById('hour12').value.trim();
+localStorage.setItem('hour12', JSON.stringify(userInput12));
+console.log(userInput12);  
+
+}
+
+document.getElementById('hour12').value = localStorage.getItem('hour12');
 
 function saveInput13(){
 
-  var userInput = document.getElementById('13').value.trim();
-  localStorage.setItem('server', JSON.stringify(userInput));
-      
-  document.getElementById('13').value = localStorage.getItem('server');
-      
-  }
+var userInput13 = document.getElementById('hour13').value.trim();
+localStorage.setItem('hour13', JSON.stringify(userInput13));
+console.log(userInput13); 
 
- function saveInput14(){
+}
 
-  var userInput = document.getElementById('14').value.trim();
-  localStorage.setItem('server', JSON.stringify(userInput));
-        
-  document.getElementById('14').value = localStorage.getItem('server');
-        
-  }
+document.getElementById('hour13').value = localStorage.getItem('hour13');
 
-  function saveInput15(){
+function saveInput14(){
 
-  var userInput = document.getElementById('15').value.trim();
-  localStorage.setItem('server', JSON.stringify(userInput));
-          
-  document.getElementById('15').value = localStorage.getItem('server');
-          
-  }
+var userInput14 = document.getElementById('hour14').value.trim();
+localStorage.setItem('hour14', JSON.stringify(userInput14));
+console.log(userInput14);
 
-  function saveInput16(){
+}
 
-    var userInput = document.getElementById('16').value.trim();
-    localStorage.setItem('server', JSON.stringify(userInput));
-            
-    document.getElementById('16').value = localStorage.getItem('server');
-            
-    }
+document.getElementById('hour14').value = localStorage.getItem('hour14');
 
-  function saveInput17(){
+function saveInput15(){
 
-    var userInput = document.getElementById('17').value.trim();
-    localStorage.setItem('server', JSON.stringify(userInput));
-      
-    document.getElementById('17').value = localStorage.getItem('server');
-      
-      }
+var userInput15 = document.getElementById('hour15').value.trim();
+localStorage.setItem('hour15', JSON.stringify(userInput15));
+console.log(userInput15); 
+
+}
+
+document.getElementById('hour15').value = localStorage.getItem('hour15');
+
+function saveInput16(){
+
+var userInput16 = document.getElementById('hour16').value.trim();
+localStorage.setItem('hour16', JSON.stringify(userInput16));
+console.log(userInput16); 
+
+}
+
+document.getElementById('hour16').value = localStorage.getItem('hour16');
+  
+function saveInput17(){
+
+var userInput17 = document.getElementById('hour17').value.trim();
+localStorage.setItem('hour17', JSON.stringify(userInput17));
+console.log(userInput17);  
+
+}
+ 
+document.getElementById('hour17').value = localStorage.getItem('hour17');
 
 $(function () {
     // TODO: Add a listener for click events on the save button. This code should
